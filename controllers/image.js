46bd0945bd75269aef714f9bba8ed4ebdadadef0
Silver Savi <silver.savi@gmail.com>
2019-09-01 15:52:00 +0300

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('Clarifai')
+
+const app = new Clarifai.App({
+ apiKey: '861ef740c4cb4bd68baa3bfe667c9662'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then(data => {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);
+	})
+	.catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+	handleImage,
+	handleApiCall
+}
